77b14d7d3248cb215ffa82810ff8776ff6bb7e0b
Pat <patrick.trudel@me.com>
2019-05-22 18:51:50 -0400

    Including some UI work & network manager work.

---
+++ TinderForPets/NetworkManager.h
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.h
+//  TinderForPets
+//
+//  Created by Patrick Trudel on 2019-05-22.
+//  Copyright © 2019 Patrick Trudel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NetworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TinderForPets/NetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.m
+//  TinderForPets
+//
+//  Created by Patrick Trudel on 2019-05-22.
+//  Copyright © 2019 Patrick Trudel. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@implementation NetworkManager
+
+@end
