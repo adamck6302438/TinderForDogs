decdea0d060479f71b5525bb359506dfd2e0b2bd
Ray Kang <adamck6302438@gmail.com>
2019-05-23 17:16:28 -0400

    added detail view but not completed yet

---
+++ TinderForPets/DetailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DetailCollectionViewCell.swift
+//  TinderForPets
+//
+//  Created by Frances ZiyiFan on 5/23/19.
+//  Copyright © 2019 Patrick Trudel. All rights reserved.
+//
+
+import UIKit
+
+class DetailCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TinderForPets/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  TinderForPets
+//
+//  Created by Frances ZiyiFan on 5/23/19.
+//  Copyright © 2019 Patrick Trudel. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
