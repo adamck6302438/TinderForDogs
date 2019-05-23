6d0869b0c52a8ab588069a8b9c96de2f78cf25b1
Ray Kang <adamck6302438@gmail.com>
2019-05-23 17:16:35 -0400

    added detail view but not completed yet

--- TinderForPets/DetailCollectionViewCell.swift
+++ TinderForPets/DetailCollectionViewCell.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class DetailCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView : UIImageView!
+    @IBOutlet weak var nameLabel : UILabel!
+    @IBOutlet weak var breedLabel : UILabel!
+    @IBOutlet weak var ageLabel : UILabel!
+    @IBOutlet weak var genderLabel : UILabel!
+    @IBOutlet weak var distanceLabel : UILabel!
+    var dogInCell : Dog!
+    
+    func setUpCell(dog : Dog) {
+        self.dogInCell = dog
+        self.nameLabel.text = dog.name
+//        print(nameLabel.text)
+        self.breedLabel.text = dog.breed
+//        print(breedLabel.text)
+        self.ageLabel.text = ("\(dog.age)")
+//        print(ageLabel.text)
+        if(dog.isMale){
+            self.genderLabel.text = "Male"
+        }else{
+            self.genderLabel.text = "Female"
+        }
+        self.distanceLabel.text = "\(dog.distance)"
+    }
 }
--- TinderForPets/DetailViewController.swift
+++ TinderForPets/DetailViewController.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController{
+    
+    var favouriteSections = ["Super Liked", "Liked"]
+    var favouriteArray = [[Dog]]()
+    var testDog = Dog.init(id: "123", name: "Dog", breed: "Lab", age: DogAge.adult, size: DogSize.extraLarge, description: "none", color: "black", isMale: true, distance: 100)
+    @IBOutlet weak var collectionView : UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //
+        User.shared.liked.append(testDog)
+        User.shared.superLiked.append(testDog)
+        
+        //
+        favouriteArray.append(User.shared.superLiked)
+        favouriteArray.append(User.shared.liked)
+        
     }
-    
-
     /*
     // MARK: - Navigation
 
@@ -26,5 +36,62 @@ class DetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
+extension DetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    //number of sections : 2
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        print("\(favouriteSections.count)")
+        return favouriteSections.count
+    }
+    
+    //number of items in section
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("\(favouriteArray[section].count)")
+        return favouriteArray[section].count
+    }
+    
+    //generate custom cell for each favourite dog
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "favourite", for: indexPath) as! DetailCollectionViewCell
+//        let dog = favouriteArray[indexPath.section][indexPath.row]
+        
+        cell.setUpCell(dog: testDog)
+        
+        //setup tap gesture recognizer
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector(("handleDoubleTap:")))
+        tapGestureRecognizer.numberOfTapsRequired = 2
+        cell.addGestureRecognizer(tapGestureRecognizer)
+        
+        //setup long press gesture recognizer
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: Selector(("handleLongPress:")))
+        cell.addGestureRecognizer(longPressGestureRecognizer)
+        return cell
+    }
+    
+    class MyCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
+    {
+        //MARK: - UICollectionViewDelegateFlowLayout
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+        {
+            return CGSize(width: self.view.frame.width/2, height: self.view.frame.height/2)
+        }
+    }
+    
+    
+    func handleDoubleTap(gesture : UITapGestureRecognizer){
+//        let pointInCollectionView : CGPoint = gesture.location(in: self.collectionView)
+//        let selectedIndexPath: IndexPath? = self.collectionView.indexPathForItem(at: pointInCollectionView)
+//        let selectedCell : DetailCollectionViewCell = self.collectionView.cellForItem(at: selectedIndexPath)
+        print("double tapped")
+    }
+    
+    //handle long press
+    func handleLongPress(gesture : UILongPressGestureRecognizer){
+        
+    }
+    
+}
+
